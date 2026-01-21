d745c5489d64fe138a50040f6d2bc6806452f831
cgpvicto-2538152 <2538152@etudiant.cegepvicto.ca>
2026-01-21 09:18:31 -0500

    Ex.1

---
+++ Exercices/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercices;
+
+/// <summary>
+/// Classe qui regroupe les méthodes des exercices à faire.
+/// </summary>
+class Program
+{
+    /// <summary>
+    /// Execute les méthodes des exercies
+    /// </summary>
+    /// <param name="args"></param>
+    public static void Main(string[] args)
+    {
+        #region Ex. 1 
+        Exercices1();
+        #endregion
+    }
+
+    /// <summary>
+    /// Affiche un message à la console.
+    /// </summary>
+    public static void Exercices1()
+    {
+        Console.WriteLine("Allô p2");
+    }
+}
